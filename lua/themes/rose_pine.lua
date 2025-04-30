6ad60a160262575b2dd68240ba922b1d000ecf23
Leon Windhorst <leon@windhorst.dev>
2025-04-30 07:46:04 +0200

    Update lualine color palette for rose-pine

--- lua/themes/rose_pine.lua
+++ lua/themes/rose_pine.lua
@@ -14,32 +14,32 @@ local function lualine_theme()
 		normal = {
 			a = { bg = p.rose, fg = p.base, gui = "bold" },
 			b = { bg = p.overlay, fg = p.rose },
-			c = { bg = nil, fg = p.text },
+			c = { bg = p.base, fg = p.text },
 		},
 		insert = {
 			a = { bg = p.foam, fg = p.base, gui = "bold" },
 			b = { bg = p.overlay, fg = p.foam },
-			c = { bg = nil, fg = p.text },
+			c = { bg = p.base, fg = p.text },
 		},
 		visual = {
 			a = { bg = p.iris, fg = p.base, gui = "bold" },
 			b = { bg = p.overlay, fg = p.iris },
-			c = { bg = nil, fg = p.text },
+			c = { bg = p.base, fg = p.text },
 		},
 		replace = {
 			a = { bg = p.pine, fg = p.base, gui = "bold" },
 			b = { bg = p.overlay, fg = p.pine },
-			c = { bg = nil, fg = p.text },
+			c = { bg = p.base, fg = p.text },
 		},
 		command = {
 			a = { bg = p.love, fg = p.base, gui = "bold" },
 			b = { bg = p.overlay, fg = p.love },
-			c = { bg = nil, fg = p.text },
+			c = { bg = p.base, fg = p.text },
 		},
 		inactive = {
 			a = { bg = p.base, fg = p.muted, gui = "bold" },
 			b = { bg = p.overlay, fg = p.muted },
-			c = { bg = nil, fg = p.muted },
+			c = { bg = p.base, fg = p.muted },
 		},
 	}
 end
